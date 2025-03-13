86c1eb48d555e5552ae06ef1976315f3de1ffe05
Jampord <cs.actubjohnford@gmail.com>
2025-03-13 14:42:50 +0800

    added condition based on access permission (final-approving)

--- src/Pages/Approving/Request/RequestApproving.jsx
+++ src/Pages/Approving/Request/RequestApproving.jsx
@@ -5,6 +5,8 @@ import { Badge, Box, Tab, Tabs, Typography } from "@mui/material";
 import PendingRequest from "./PendingRequest";
 import ApprovedRequest from "./ApprovedRequest";
 import { useGetNotificationApiQuery } from "../../../Redux/Query/Notification";
+import { useSelector } from "react-redux";
+import FinalApproval from "./FinalApproval";
 
 const RequestApproving = () => {
   const [value, setValue] = useState("1");
@@ -19,6 +21,8 @@ const RequestApproving = () => {
     setValue(newValue);
   };
 
+  const permissions = useSelector((state) => state.userLogin?.user.role.access_permission);
+
   return (
     <Box className="mcontainer">
       <Typography className="mcontainer__title" sx={{ fontFamily: "Anton", fontSize: "1.6rem" }}>
@@ -26,29 +30,68 @@ const RequestApproving = () => {
       </Typography>
 
       <Box>
-        <TabContext value={value}>
-          <Tabs onChange={handleChange} value={value}>
-            <Tab
-              label={
-                <Badge color="error" badgeContent={notifData?.toApproveCount}>
-                  Pending Request
-                </Badge>
-              }
-              value="1"
-              className={value === "1" ? "tab__background" : null}
-            />
+        {permissions.includes("final-approving") ? (
+          <TabContext value={value}>
+            <Tabs onChange={handleChange} value={value}>
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.toApproveCount}>
+                    First Approving
+                  </Badge>
+                }
+                value="1"
+                className={value === "1" ? "tab__background" : null}
+              />
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.toAcquisitionFaApproval}>
+                    Second Approving
+                  </Badge>
+                }
+                value="2"
+                className={value === "2" ? "tab__background" : null}
+              />
 
-            <Tab label="Approved Request" value="2" className={value === "2" ? "tab__background" : null} />
-          </Tabs>
+              <Tab label="Approved Request" value="3" className={value === "3" ? "tab__background" : null} />
+            </Tabs>
 
-          <TabPanel sx={{ p: 0 }} value="1" index="1">
-            <PendingRequest refetch />
-          </TabPanel>
+            <TabPanel sx={{ p: 0 }} value="1" index="1">
+              <FinalApproval />
+            </TabPanel>
 
-          <TabPanel sx={{ p: 0 }} value="2" index="2">
-            <ApprovedRequest />
-          </TabPanel>
-        </TabContext>
+            <TabPanel sx={{ p: 0 }} value="2" index="2">
+              <FinalApproval final />
+            </TabPanel>
+
+            <TabPanel sx={{ p: 0 }} value="3" index="3">
+              <ApprovedRequest />
+            </TabPanel>
+          </TabContext>
+        ) : (
+          <TabContext value={value}>
+            <Tabs onChange={handleChange} value={value}>
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.toApproveCount}>
+                    Pending Request
+                  </Badge>
+                }
+                value="1"
+                className={value === "1" ? "tab__background" : null}
+              />
+
+              <Tab label="Approved Request" value="2" className={value === "2" ? "tab__background" : null} />
+            </Tabs>
+
+            <TabPanel sx={{ p: 0 }} value="1" index="1">
+              <PendingRequest refetch />
+            </TabPanel>
+
+            <TabPanel sx={{ p: 0 }} value="2" index="2">
+              <ApprovedRequest />
+            </TabPanel>
+          </TabContext>
+        )}
       </Box>
     </Box>
   );
