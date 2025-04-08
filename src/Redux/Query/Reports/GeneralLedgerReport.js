90bc14a657ed9f6bb03d43627f86d6c54e29f698
Jampord <cs.actubjohnford@gmail.com>
2025-04-08 13:54:20 +0800

    feat: add API slice for General Ledger Reports with query endpoints

---
+++ src/Redux/Query/Reports/GeneralLedgerReport.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const generalLedgerReportApi = createApi({
+  reducerPath: "generalLedgerReportApi",
+  tagTypes: ["GeneralLedgerReport"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.VLADIMIR_BASE_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("token");
+
+      headers.set("x-api-key", `${process.env.GL_KEY}`);
+      headers.set("Accept", `application/json`);
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getGeneralLedgerReportApi: builder.query({
+      query: (params) =>
+        `report/gl-report?adjustment_month=${params.adjustment_date}&per_page=${params.per_page}&page=${params.page}`,
+      providesTags: ["GeneralLedgerReport"],
+    }),
+    getExportGeneralLedgerReportApi: builder.query({
+      query: (params) => `report/gl-report?adjustment_month=${params.adjustment_date}&pagination=none`,
+      providesTags: ["GeneralLedgerReport"],
+    }),
+  }),
+});
+
+export const { useGetGeneralLedgerReportApiQuery, useLazyGetExportGeneralLedgerReportApiQuery } =
+  generalLedgerReportApi;
