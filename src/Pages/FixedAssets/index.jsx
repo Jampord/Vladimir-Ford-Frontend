edc37c3e8490bd958ddf119f038fcfc5c3b8dddc
Jampord <cs.actubjohnford@gmail.com>
2025-04-14 13:30:43 +0800

    feat: add FixedAssetIndex component for managing fixed assets and navigation

---
+++ src/Pages/FixedAssets/index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+import "../../Style/parentSidebar.scss";
+import { useLocation } from "react-router-dom";
+
+import { Box, Typography, useMediaQuery } from "@mui/material";
+import { InventoryRounded, PriceChange } from "@mui/icons-material";
+import Cards from "../../Components/Reusable/Cards";
+
+const FixedAssetList = [
+  {
+    icon: <InventoryRounded />,
+    label: "Fixed Assets",
+    description: "List of Fixed Assets",
+    path: "/fixed-asset/fixed-asset",
+  },
+
+  {
+    icon: <PriceChange />,
+    label: "Depreciation",
+    description: "List of Fixed Asset Depreciation",
+    path: "/fixed-asset/depreciation",
+  },
+];
+
+const FixedAssetIndex = () => {
+  const location = useLocation();
+  const isSmallScreen = useMediaQuery("(max-width: 590px)");
+  // console.log(location);
+
+  return (
+    <>
+      {location.pathname === "/fixed-asset" ? (
+        <>
+          <Typography
+            color="secondary"
+            sx={{
+              fontFamily: "Anton, Roboto, Impact, Helvetica",
+              fontSize: "25px",
+              alignSelf: isSmallScreen ? "center" : "flex-start",
+              marginLeft: isSmallScreen ? null : "30px",
+            }}
+          >
+            Fixed Asset
+          </Typography>
+          <Box className="parentSidebar">
+            <Box className="parentSidebar__container">
+              <Box className="parentSidebar__wrapper">
+                {FixedAssetList?.map((data, index) => {
+                  return <Cards data={data} key={index} />;
+                })}
+              </Box>
+            </Box>
+          </Box>
+        </>
+      ) : (
+        <Outlet />
+      )}
+    </>
+    // <Outlet />
+  );
+};
+
+export default FixedAssetIndex;
