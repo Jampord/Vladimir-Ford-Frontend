f5df33c5cac423cbaea974dabfed78c5152918b3
Jampord <cs.actubjohnford@gmail.com>
2025-04-25 13:49:19 +0800

    feat: add date selection and export functionality to PurchaseRequestRecon component

--- src/Pages/Reports/PurchaseRequestRecon.jsx
+++ src/Pages/Reports/PurchaseRequestRecon.jsx
@@ -1,6 +1,9 @@
 import {
   Box,
+  Button,
   Chip,
+  Dialog,
+  Grow,
   Stack,
   Table,
   TableBody,
@@ -9,6 +12,7 @@ import {
   TableHead,
   TableRow,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import MasterlistToolbar from "../../Components/Reusable/MasterlistToolbar";
 import { useState } from "react";
@@ -17,11 +21,19 @@ import NoRecordsFound from "../../Layout/NoRecordsFound";
 import { LoadingData } from "../../Components/LottieFiles/LottieComponents";
 import MasterlistSkeleton from "../Skeleton/MasterlistSkeleton";
 import ErrorFetching from "../ErrorFetching";
+import CustomTablePagination from "../../Components/Reusable/CustomTablePagination";
+import { IosShareRounded } from "@mui/icons-material";
+import { openExport } from "../../Redux/StateManagement/booleanStateSlice";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
+import { DatePicker } from "@mui/x-date-pickers";
+import ExportPurchaseRequestRecon from "./ExportPurchaseRequestRecon";
 
 const PurchaseRequestRecon = () => {
   const [search, setSearch] = useState("");
-  const [perPage, setPerPage] = useState(5);
+  const [perPage, setPerPage] = useState(25);
   const [page, setPage] = useState(1);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const perPageHandler = (e) => {
     setPage(1);
@@ -33,6 +45,9 @@ const PurchaseRequestRecon = () => {
     setPage(page + 1);
   };
 
+  const dispatch = useDispatch();
+  const showExport = useSelector((state) => state.booleanState.exportFile);
+
   const {
     data: purchaseRequestReconData,
     isLoading: ispurchaseRequestReconLoading,
@@ -41,12 +56,19 @@ const PurchaseRequestRecon = () => {
     isFetching: ispurchaseRequestReconFetching,
     error: errorData,
     refetch,
-  } = useGetPurchaseRequestReconQuery({}, { refetchOnMountOrArgChange: true });
+  } = useGetPurchaseRequestReconQuery(
+    {
+      year_month: selectedDate !== null ? moment(selectedDate).format("YYYY-MM") : moment(new Date()).format("YYYY-MM"),
+      page: page,
+      per_page: perPage,
+      pagination: "",
+    },
+    { refetchOnMountOrArgChange: true }
+  );
 
   console.log("purchaseRequestReconData", purchaseRequestReconData);
 
   const transactionStatus = (data) => {
-    console.log({ data });
     let statusColor, hoverColor, textColor, variant;
 
     switch (data.status) {
@@ -97,6 +119,12 @@ const PurchaseRequestRecon = () => {
     );
   };
 
+  const openExportDialog = () => {
+    dispatch(openExport());
+  };
+
+  const isSmallerScreen = useMediaQuery("(max-width: 600px)");
+
   return (
     <Box className="mcontainer">
       <Typography className="mcontainer__title" sx={{ fontFamily: "Anton", fontSize: "1.6rem" }}>
@@ -106,59 +134,118 @@ const PurchaseRequestRecon = () => {
       {ispurchaseRequestReconLoading && <MasterlistSkeleton onAdd={false} />}
       {ispurchaseRequestReconError && <ErrorFetching refetch={refetch} error={errorData} />}
       {purchaseRequestReconData && !ispurchaseRequestReconError && (
-        <Stack height="100%">
-          <Box className="mcontainer__wrapper">
-            <MasterlistToolbar onSearchChange={setSearch} onSetPage={setPage} hideArchive />
+        // <Stack height="100%">
+        <Box className="mcontainer__wrapper">
+          {/* <MasterlistToolbar onSearchChange={setSearch} onSetPage={setPage} hideArchive /> */}
+          <Stack
+            flexDirection="row"
+            gap={isSmallerScreen ? 1 : 2}
+            justifyContent="right"
+            flexWrap={isSmallerScreen ? "wrap" : null}
+            mt={1}
+          >
+            <DatePicker
+              label={"Month and Year"}
+              views={["month", "year"]}
+              maxDate={new Date()}
+              value={selectedDate || new Date()} // Bind the state to the DatePicker
+              onChange={(newValue) => setSelectedDate(newValue)} // Update state on change
+              // slotProps={{
+              //   textField: {
+              //     helperText: selectedDate === null && "Select a Month and Year to fetch data.",
+              //     FormHelperTextProps: {
+              //       style: { color: "red" }, // Change helper text color to red
+              //     },
+              //   },
+              // }}
+            />
+          </Stack>
 
-            <Box>
-              <TableContainer className="mcontainer__th-body">
-                <Table className="mcontainer__table" stickyHeader>
-                  <TableHead>
-                    <TableRow
-                      sx={{
-                        "& > *": {
-                          fontWeight: "bold!important",
-                          whiteSpace: "nowrap",
-                        },
-                      }}
-                    >
-                      <TableCell className="tbl-cell text-center">Ymir PR</TableCell>
-                      <TableCell className="tbl-cell text-center">Vladimir PR</TableCell>
-                      <TableCell className="tbl-cell text-center">Status</TableCell>
-                    </TableRow>
-                  </TableHead>
+          <Box>
+            <TableContainer className="mcontainer__th-body">
+              <Table className="mcontainer__table" stickyHeader>
+                <TableHead>
+                  <TableRow
+                    sx={{
+                      "& > *": {
+                        fontWeight: "bold!important",
+                        whiteSpace: "nowrap",
+                      },
+                    }}
+                  >
+                    <TableCell className="tbl-cell text-center">Ymir PR</TableCell>
+                    <TableCell className="tbl-cell text-center">Vladimir PR</TableCell>
+                    <TableCell className="tbl-cell text-center">Status</TableCell>
+                  </TableRow>
+                </TableHead>
 
-                  <TableBody>
-                    {purchaseRequestReconData?.length === 0 ? (
-                      <NoRecordsFound />
-                    ) : ispurchaseRequestReconLoading || ispurchaseRequestReconFetching ? (
-                      <LoadingData />
-                    ) : (
-                      ispurchaseRequestReconSuccess &&
-                      purchaseRequestReconData.map((data, index) => {
-                        return (
-                          <TableRow
-                            key={index}
-                            sx={{
-                              "&:last-child td, &:last-child th": {
-                                borderBottom: 0,
-                              },
-                            }}
-                          >
-                            <TableCell className="tbl-cell text-center">{data.ymir_pr}</TableCell>
-                            <TableCell className="tbl-cell text-center">{data.vlad_pr}</TableCell>
-                            <TableCell className="tbl-cell text-center">{transactionStatus(data)}</TableCell>
-                          </TableRow>
-                        );
-                      })
-                    )}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Box>
+                <TableBody>
+                  {purchaseRequestReconData?.data.length === 0 ? (
+                    <NoRecordsFound heightData="medium" />
+                  ) : ispurchaseRequestReconLoading || ispurchaseRequestReconFetching ? (
+                    <LoadingData />
+                  ) : (
+                    ispurchaseRequestReconSuccess &&
+                    purchaseRequestReconData?.data.map((data, index) => {
+                      return (
+                        <TableRow
+                          key={index}
+                          sx={{
+                            "&:last-child td, &:last-child th": {
+                              borderBottom: 0,
+                            },
+                          }}
+                        >
+                          <TableCell className="tbl-cell text-center">{data.ymir_pr}</TableCell>
+                          <TableCell className="tbl-cell text-center">{data.vlad_pr}</TableCell>
+                          <TableCell className="tbl-cell text-center">{transactionStatus(data)}</TableCell>
+                        </TableRow>
+                      );
+                    })
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
-        </Stack>
+
+          <Box className="mcontainer__pagination-export">
+            <Button
+              className="mcontainer__export"
+              variant="outlined"
+              size="small"
+              color="text"
+              startIcon={<IosShareRounded color="primary" />}
+              onClick={openExportDialog}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: "10px 20px",
+              }}
+            >
+              EXPORT
+            </Button>
+
+            <CustomTablePagination
+              total={purchaseRequestReconData?.total}
+              success={purchaseRequestReconData}
+              current_page={purchaseRequestReconData?.current_page}
+              per_page={purchaseRequestReconData?.per_page}
+              onPageChange={pageHandler}
+              onRowsPerPageChange={perPageHandler}
+            />
+          </Box>
+        </Box>
+        // </Stack>
       )}
+
+      <Dialog
+        open={showExport}
+        TransitionComponent={Grow}
+        PaperProps={{ sx: { maxWidth: "1320px", borderRadius: "10px", p: 3 } }}
+      >
+        <ExportPurchaseRequestRecon />
+      </Dialog>
     </Box>
   );
 };
