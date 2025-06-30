8cc9dcefc5c53c05572f16ed851bdc6a1fa94f4e
Jampord <cs.actubjohnford@gmail.com>
2025-06-30 18:01:28 +0800

    feat: update ExportFixedAssetDepreciation component to specify type for depreciation

--- src/Pages/FixedAssets/Depreciation/ForDepreciation.jsx
+++ src/Pages/FixedAssets/Depreciation/ForDepreciation.jsx
@@ -320,7 +320,7 @@ const ForDepreciation = () => {
           },
         }}
       >
-        <ExportFixedAssetDepreciation />
+        <ExportFixedAssetDepreciation type={"for depreciation"} />
       </Dialog>
     </Stack>
   );
