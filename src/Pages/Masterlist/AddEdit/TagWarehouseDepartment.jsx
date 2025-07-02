6715bab75b010123c70defbcf4741fcdb5381050
Jampord <cs.actubjohnford@gmail.com>
2025-07-02 16:21:57 +0800

    feat: update TagWarehouseDepartment to use one_charging_id for improved tagging functionality

--- src/Pages/Masterlist/AddEdit/TagWarehouseDepartment.jsx
+++ src/Pages/Masterlist/AddEdit/TagWarehouseDepartment.jsx
@@ -2,7 +2,7 @@ import { LoadingButton } from "@mui/lab";
 import { Box, Button, Checkbox, TextField, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { closeDrawer } from "../../../Redux/StateManagement/booleanStateSlice";
-import { usePutWarehouseDepartmentTaggingApiMutation } from "../../../Redux/Query/Masterlist/Warehouse";
+import { usePutWarehouseOneChargingTaggingApiMutation } from "../../../Redux/Query/Masterlist/Warehouse";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -12,9 +12,10 @@ import CustomTextField from "../../../Components/Reusable/CustomTextField";
 import { openToast } from "../../../Redux/StateManagement/toastSlice";
 import { useEffect } from "react";
 import { useGetDepartmentAllApiQuery } from "../../../Redux/Query/Masterlist/YmirCoa/Department";
+import { useGetOneRDFChargingAllApiQuery } from "../../../Redux/Query/Masterlist/OneRDF/OneRDFCharging";
 
 const schema = yup.object().shape({
-  department_id: yup.array().required().label("Department").typeError("Department is required"),
+  one_charging_id: yup.array().required().label("Department").typeError("Department is required"),
   warehouse_name: yup.string().nullable().label("Warehouse Name"),
 });
 
@@ -28,7 +29,7 @@ const TagWarehouseDepartment = ({ data }) => {
   const [
     putWarehouseDepartment,
     { data: postData, isLoading: isPostLoading, isSuccess: isPostSuccess, isError: isPostError, error: postError },
-  ] = usePutWarehouseDepartmentTaggingApiMutation();
+  ] = usePutWarehouseOneChargingTaggingApiMutation();
 
   const {
     data: departmentData = [],
@@ -36,9 +37,9 @@ const TagWarehouseDepartment = ({ data }) => {
     isSuccess: isDepartmentSuccess,
     isError: isDepartmentError,
     refetch: isDepartmentRefetch,
-  } = useGetDepartmentAllApiQuery({ refetchOnMountOrArgChange: true });
+  } = useGetOneRDFChargingAllApiQuery({ pagination: "none" }, { refetchOnMountOrArgChange: true });
 
-  // console.log("departmentData", departmentData);
+  console.log("departmentData", departmentData);
 
   const {
     handleSubmit,
@@ -51,7 +52,7 @@ const TagWarehouseDepartment = ({ data }) => {
   } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      department_id: data?.department || [],
+      one_charging_id: data?.one_charging || [],
       warehouse_name: data?.warehouse_name || "",
     },
   });
@@ -62,9 +63,9 @@ const TagWarehouseDepartment = ({ data }) => {
 
   const onSubmitHandler = (formData) => {
     console.log("formData", formData);
-    const department_id = formData.department_id.map((item) => item.sync_id);
-    // console.log("department_idddddd", { department_id: department_id });
-    putWarehouseDepartment({ id: data?.sync_id, department_id: department_id });
+    const one_charging_id = formData.one_charging_id.map((item) => item.sync_id);
+    console.log("one_charging_idddddd", { one_charging_id: one_charging_id });
+    putWarehouseDepartment({ id: data?.sync_id, one_charging_id: one_charging_id });
   };
 
   useEffect(() => {
@@ -98,14 +99,14 @@ const TagWarehouseDepartment = ({ data }) => {
     }
   }, [isPostError]);
 
-  // console.log("👀👀👀", watch("department_id"));
+  // console.log("👀👀👀", watch("one_charging_id"));
 
   return (
     <Box className="add-masterlist">
       <Typography color="secondary.main" sx={{ fontFamily: "Anton", fontSize: "1.5rem" }}>
         {data.action === "view"
           ? "View Warehouse Tagging"
-          : data?.department.length === 0
+          : data?.one_charging.length === 0
           ? "Add Warehouse Tagging"
           : "Edit Warehouse Tagging"}
       </Typography>
@@ -114,20 +115,21 @@ const TagWarehouseDepartment = ({ data }) => {
         <CustomAutoComplete
           readOnly={data.action === "view"}
           // freeSolo={data.action === "view"}
-          disabled={data.action === "view" && data.department.length === 0}
+          disabled={data.action === "view" && data.one_charging.length === 0}
           autoComplete
           multiple
           disableCloseOnSelect
-          name="department_id"
+          name="one_charging_id"
           control={control}
           options={departmentData}
           loading={isDepartmentLoading}
           size="small"
-          getOptionLabel={(option) => `${option.department_code} - ${option.department_name}`}
+          getOptionLabel={(option) => `${option.code} - ${option.name}`}
           getOptionKey={(option, index) => `${option.id}-${index}`}
           //   getOptionDisabled={(option) => !data?.department?.some((item) => item?.id !== option?.id)}
           getOptionDisabled={(option) =>
-            option?.warehouse !== null && !data?.department?.some((item) => item?.sync_id === option?.sync_id)
+            option?.receiving_warehouse !== null &&
+            !data?.one_charging?.some((item) => item?.sync_id === option?.sync_id)
           }
           isOptionEqualToValue={(option, value) => option.sync_id === value.sync_id}
           slotProps={{
@@ -159,17 +161,20 @@ const TagWarehouseDepartment = ({ data }) => {
           }}
           renderOption={(props, option, { selected }) => (
             <li {...props}>
+              {console.log("option", option)}
               <Checkbox icon={icon} checkedIcon={checkedIcon} style={{ marginRight: 8 }} checked={selected} />
-              {`${option.department_code} - ${option.department_name}`}
+              {`${option.code} - ${option.name}`}
             </li>
           )}
           renderInput={(params) => (
             <TextField
               color="secondary"
               {...params}
-              label={data.action === "view" && data.department.length === 0 ? "No Tagged Department" : "Department"}
-              error={!!errors?.department_id?.message}
-              helperText={errors?.department_id?.message}
+              label={
+                data.action === "view" && data.one_charging.length === 0 ? "No tagged One Charging" : "One Charging"
+              }
+              error={!!errors?.one_charging_id?.message}
+              helperText={errors?.one_charging_id?.message}
             />
           )}
         />
@@ -193,10 +198,10 @@ const TagWarehouseDepartment = ({ data }) => {
             variant="contained"
             size="small"
             loading={isPostLoading}
-            disabled={!isValid || watch("department_id").length === 0}
+            disabled={!isValid || watch("one_charging_id").length === 0}
             sx={data.action === "view" ? { display: "none" } : null}
           >
-            {data?.department.length === 0 ? "Tag" : "Update"}
+            {data?.one_charging.length === 0 ? "Tag" : "Update"}
           </LoadingButton>
 
           <Button
