410725abfc3734bcd0131da705ad3ae8b6f8c463
Jampord <cs.actubjohnford@gmail.com>
2025-07-04 16:57:55 +0800

    feat: enhance FixedAssetDepreciationView to display requestor's full name in bold for improved visibility

--- src/Pages/FixedAssets/Depreciation/FixedAssetDepreciationView.jsx
+++ src/Pages/FixedAssets/Depreciation/FixedAssetDepreciationView.jsx
@@ -347,7 +347,12 @@ const FixedAssetDepreciationView = (props) => {
                       <Typography fontSize="14px" color={"secondary.main"}>
                         User:
                       </Typography>
-                      <Typography className="tableCard__infoCapex" fontSize="14px" color={"secondary.main"}>
+                      <Typography
+                        className="tableCard__infoCapex"
+                        fontSize="14px"
+                        fontWeight={700}
+                        color={"secondary.main"}
+                      >
                         {dataApi?.data?.requestor?.employee_id} {dataApi?.data?.requestor?.first_name}{" "}
                         {dataApi?.data?.requestor?.last_name}
                       </Typography>
