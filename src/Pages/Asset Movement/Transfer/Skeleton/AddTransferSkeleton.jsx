f157ae4bd3f6f7588aeb1074209aec719c22d495
Jampord <cs.actubjohnford@gmail.com>
2025-07-24 16:32:09 +0800

    feat: add skeleton component for Add Transfer to enhance loading experience

---
+++ src/Pages/Asset Movement/Transfer/Skeleton/AddTransferSkeleton.jsx
@@ -0,0 +1,53 @@
+import { Box, Skeleton, Stack } from "@mui/material";
+
+const AddTransferSkeleton = () => {
+  return (
+    <Stack>
+      <Skeleton variant="text" width={160} height={60} />
+
+      <Box>
+        <Skeleton variant="text" width={100} height={35} />
+      </Box>
+
+      <Stack direction="column" spacing={1} mt={1}>
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+      </Stack>
+
+      <Stack mt={3}>
+        <Skeleton variant="text" width={110} height={35} />
+      </Stack>
+
+      <Stack direction="column" spacing={1} mt={1}>
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Skeleton variant="rounded" width={210} height={30} />
+      </Stack>
+
+      <Stack mt={3}>
+        <Skeleton variant="text" width={110} height={35} />
+      </Stack>
+      <Stack direction="column" spacing={1} mt={1}>
+        <Skeleton variant="rounded" width={210} height={30} />
+      </Stack>
+
+      <Stack mt={2}>
+        <Skeleton variant="text" width={120} height={40} />
+      </Stack>
+
+      <Stack direction="column" mt={1}>
+        <Skeleton variant="rounded" width={210} height={30} />
+        <Stack gap={0.2} mt={0.5}>
+          <Skeleton variant="text" width={180} height={10} />
+          <Skeleton variant="text" width={180} height={10} />
+          <Skeleton variant="text" width={180} height={10} />
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default AddTransferSkeleton;
