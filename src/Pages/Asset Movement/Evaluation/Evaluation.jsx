f26d9c02a3d109d0cb7f703b51fc2ef8a4e8a841
Jampord <cs.actubjohnford@gmail.com>
2025-08-18 16:32:41 +0800

    feat: add ForReplacement tab to Evaluation component and update tab structure

--- src/Pages/Asset Movement/Evaluation/Evaluation.jsx
+++ src/Pages/Asset Movement/Evaluation/Evaluation.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import ToPickup from "./ToPickup";
 import ListOfPullout from "./ListOfPullout";
 import ForApprovalEvaluation from "./ForApprovalEvaluation";
+import ForReplacement from "./ForReplacement";
 
 const Evaluation = () => {
   const [value, setValue] = useState("1");
@@ -23,8 +24,9 @@ const Evaluation = () => {
           <Tabs onChange={handleChange} value={value}>
             <Tab label="To Pickup" value="1" className={value === "1" ? "tab__background" : null} />
             <Tab label="List of Pullout" value="2" className={value === "2" ? "tab__background" : null} />
-            <Tab label="For Approval" value="3" className={value === "3" ? "tab__background" : null} />
-            <Tab label="Approved" value="4" className={value === "4" ? "tab__background" : null} />
+            {/* <Tab label="For Approval" value="3" className={value === "3" ? "tab__background" : null} /> */}
+            <Tab label="Repaired" value="4" className={value === "4" ? "tab__background" : null} />
+            <Tab label="For Replacement" value="5" className={value === "5" ? "tab__background" : null} />
           </Tabs>
 
           <TabPanel sx={{ p: 0 }} value="1" index="1">
@@ -35,12 +37,15 @@ const Evaluation = () => {
             <ListOfPullout />
           </TabPanel>
 
-          <TabPanel sx={{ p: 0 }} value="3" index="3">
+          {/* <TabPanel sx={{ p: 0 }} value="3" index="3">
             <ForApprovalEvaluation tab={value} />
-          </TabPanel>
+          </TabPanel> */}
 
           <TabPanel sx={{ p: 0 }} value="4" index="4">
             <ForApprovalEvaluation tab={value} />
+          </TabPanel>
+          <TabPanel sx={{ p: 0 }} value="5" index="5">
+            <ForReplacement />
           </TabPanel>
         </TabContext>
       </Box>
