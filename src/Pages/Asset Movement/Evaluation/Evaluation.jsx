716aa94e821674b95f32410ad71d190c2f63da37
Jampord <cs.actubjohnford@gmail.com>
2025-08-29 15:43:26 +0800

    feat: add permission-based tabs for Replacement, Spare, and Disposal in Evaluation component

--- src/Pages/Asset Movement/Evaluation/Evaluation.jsx
+++ src/Pages/Asset Movement/Evaluation/Evaluation.jsx
@@ -13,6 +13,11 @@ const Evaluation = () => {
   const handleChange = (_, newValue) => {
     setValue(newValue);
   };
+
+  const user = JSON.parse(localStorage.getItem("user")) || {};
+  const hasPermission = user?.role?.access_permission.includes(
+    "mis-warehouse" || "fixed-asset-warehouse" || "engineering-warehouse"
+  );
   return (
     <Box className="mcontainer">
       <Typography className="mcontainer__title" sx={{ fontFamily: "Anton", fontSize: "1.6rem" }}>
@@ -26,7 +31,13 @@ const Evaluation = () => {
             <Tab label="List of Pullout" value="2" className={value === "2" ? "tab__background" : null} />
             {/* <Tab label="For Approval" value="3" className={value === "3" ? "tab__background" : null} /> */}
             <Tab label="Repaired" value="4" className={value === "4" ? "tab__background" : null} />
-            <Tab label="For Replacement" value="5" className={value === "5" ? "tab__background" : null} />
+            {hasPermission && (
+              <Tab label="For Replacement" value="5" className={value === "5" ? "tab__background" : null} />
+            )}
+            {hasPermission && <Tab label="Spare" value="6" className={value === "6" ? "tab__background" : null} />}
+            {hasPermission && (
+              <Tab label="For Disposal" value="7" className={value === "7" ? "tab__background" : null} />
+            )}
           </Tabs>
 
           <TabPanel sx={{ p: 0 }} value="1" index="1">
@@ -45,7 +56,13 @@ const Evaluation = () => {
             <ForApprovalEvaluation tab={value} />
           </TabPanel>
           <TabPanel sx={{ p: 0 }} value="5" index="5">
-            <ForReplacement />
+            <ForReplacement tab={value} />
+          </TabPanel>
+          <TabPanel sx={{ p: 0 }} value="6" index="6">
+            <ForReplacement tab={value} />
+          </TabPanel>
+          <TabPanel sx={{ p: 0 }} value="7" index="7">
+            <ForReplacement tab={value} />
           </TabPanel>
         </TabContext>
       </Box>
