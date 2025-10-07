ac6980fcd09d5fd4d565e059574ad09df4cbfa4b
Jampord <cs.actubjohnford@gmail.com>
2025-10-07 15:49:13 +0800

    feat: add additional endpoints on disposal api

--- src/Redux/Query/Movement/Disposal.js
+++ src/Redux/Query/Movement/Disposal.js
@@ -24,8 +24,68 @@ export const disposalApi = createApi({
 
     getDisposalByIdApi: builder.query({
       query: ({ id }) => `/disposal/${id}`,
+      providesTags: ["Disposal"],
+    }),
+
+    getDisposalApprovalApi: builder.query({
+      query: (params) => ({ url: `disposal-approver`, params }),
+      providesTags: ["Disposal"],
+    }),
+
+    patchDisposalApprovalStatusApi: builder.mutation({
+      query: (body) => ({
+        url: `/handle-disposal-movement`,
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: ["Disposal"],
+    }),
+
+    getNextDisposal: builder.query({
+      query: () => `/get-next-disposal`,
+      providesTags: ["Disposal"],
+    }),
+
+    getDisposalReceivingApi: builder.query({
+      query: (params) => ({ url: `disposal-receiver`, params }),
+      providesTags: ["Disposal"],
+    }),
+
+    patchDisposalReceivingApi: builder.mutation({
+      query: (body) => ({
+        url: `/disposal-receiving`,
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: ["Disposal"],
+    }),
+
+    deleteDisposalApi: builder.mutation({
+      query: ({ id }) => ({
+        url: `remove-disposal/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Disposal"],
+    }),
+
+    deleteDisposalItemApi: builder.mutation({
+      query: ({ movement_id, disposal_id }) => ({
+        url: `remove-disposal/${movement_id}/${disposal_id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Disposal"],
     }),
   }),
 });
 
-export const { useGetDisposalApiQuery, useGetDisposalByIdApiQuery } = disposalApi;
+export const {
+  useGetDisposalApiQuery,
+  useGetDisposalByIdApiQuery,
+  useGetDisposalApprovalApiQuery,
+  usePatchDisposalApprovalStatusApiMutation,
+  useLazyGetNextDisposalQuery,
+  useGetDisposalReceivingApiQuery,
+  usePatchDisposalReceivingApiMutation,
+  useDeleteDisposalApiMutation,
+  useDeleteDisposalItemApiMutation,
+} = disposalApi;
