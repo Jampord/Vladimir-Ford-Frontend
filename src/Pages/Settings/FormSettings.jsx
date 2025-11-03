545f46d65c5f9c9fb9385fb6dd912f2283663d0d
Jampord <cs.actubjohnford@gmail.com>
2025-11-03 09:16:33 +0800

    feat: add capex approvers setup

--- src/Pages/Settings/FormSettings.jsx
+++ src/Pages/Settings/FormSettings.jsx
@@ -6,6 +6,10 @@ import AssetPullout from "./AssetPullout";
 import AssetDisposal from "./AssetDisposal";
 import AssetEvaluation from "./AssetEvaluation";
 import AssetTransferPullout from "./AssetTransferPullout";
+import CapexApprovers from "./CapexApprovers";
+import EstimationApprovers from "./EstimationApprovers";
+import SubCapexApprovers from "./SubCapexApprovers";
+import AdditionalCostApprovers from "./AdditionalCostApprovers";
 
 const FormSettings = () => {
   const [value, setValue] = useState("option1");
@@ -21,6 +25,10 @@ const FormSettings = () => {
     option4: <AssetPullout />,
     option5: <AssetEvaluation />,
     option6: <AssetDisposal />,
+    option7: <CapexApprovers />,
+    option8: <EstimationApprovers />,
+    option9: <SubCapexApprovers />,
+    option10: <AdditionalCostApprovers />,
   };
 
   return (
@@ -51,6 +59,10 @@ const FormSettings = () => {
           <MenuItem value="option4">Asset Pullout</MenuItem>
           <MenuItem value="option5">Asset Evaluation</MenuItem>
           <MenuItem value="option6">Asset Disposal</MenuItem>
+          <MenuItem value="option7">Capex</MenuItem>
+          <MenuItem value="option8">Capex Estimation</MenuItem>
+          <MenuItem value="option9">Sub Capex</MenuItem>
+          <MenuItem value="option10">Additional Cost (Capex)</MenuItem>
         </Select>
         {views[value]}
       </Stack>
