93121dd901b018fc1b71c53bee09f5f00c7847c2
Jampord <cs.actubjohnford@gmail.com>
2025-11-03 08:39:33 +0800

    feat: add notification badges to tabs in Evaluation component

--- src/Pages/Asset Movement/Evaluation/Evaluation.jsx
+++ src/Pages/Asset Movement/Evaluation/Evaluation.jsx
@@ -1,6 +1,6 @@
 import { TabContext, TabPanel } from "@mui/lab";
-import { Box, Tab, Tabs, Typography } from "@mui/material";
-import { useState } from "react";
+import { Badge, Box, Tab, Tabs, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 import ToPickup from "./ToPickup";
 import ListOfPullout from "./ListOfPullout";
@@ -8,12 +8,19 @@ import ForApprovalEvaluation from "./ForApprovalEvaluation";
 import ForReplacement from "./ForReplacement";
 import { useDispatch, useSelector } from "react-redux";
 import { setEvaluationTabValue } from "../../../Redux/StateManagement/tabSlice";
+import { useGetNotificationApiQuery } from "../../../Redux/Query/Notification";
 
 const Evaluation = () => {
   // const [value, setValue] = useState("1");
 
   const dispatch = useDispatch();
   const value = useSelector((state) => state.tab.evaluationTabValue);
+
+  const { data: notifData, refetch } = useGetNotificationApiQuery();
+
+  useEffect(() => {
+    refetch();
+  }, [notifData]);
 
   const handleChange = (_, newValue) => {
     // setValue(newValue);
@@ -33,16 +40,58 @@ const Evaluation = () => {
       <Box>
         <TabContext value={value}>
           <Tabs onChange={handleChange} value={value}>
-            <Tab label="To Pickup" value="1" className={value === "1" ? "tab__background" : null} />
-            <Tab label="List of Pullout" value="2" className={value === "2" ? "tab__background" : null} />
+            <Tab
+              label={
+                <Badge color="error" badgeContent={notifData?.toPickUpCount}>
+                  To Pickup
+                </Badge>
+              }
+              value="1"
+              className={value === "1" ? "tab__background" : null}
+            />
+            <Tab
+              label={
+                <Badge color="error" badgeContent={notifData?.toEvaluateCount}>
+                  List of Pullout
+                </Badge>
+              }
+              value="2"
+              className={value === "2" ? "tab__background" : null}
+            />
             {/* <Tab label="For Approval" value="3" className={value === "3" ? "tab__background" : null} /> */}
             <Tab label="Repaired" value="4" className={value === "4" ? "tab__background" : null} />
             {hasPermission && (
-              <Tab label="For Replacement" value="5" className={value === "5" ? "tab__background" : null} />
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.toReplaceCount}>
+                    For Replacement
+                  </Badge>
+                }
+                value="5"
+                className={value === "5" ? "tab__background" : null}
+              />
             )}
-            {hasPermission && <Tab label="Spare" value="6" className={value === "6" ? "tab__background" : null} />}
             {hasPermission && (
-              <Tab label="For Disposal" value="7" className={value === "7" ? "tab__background" : null} />
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.spareCount}>
+                    Spare
+                  </Badge>
+                }
+                value="6"
+                className={value === "6" ? "tab__background" : null}
+              />
+            )}
+            {hasPermission && (
+              <Tab
+                label={
+                  <Badge color="error" badgeContent={notifData?.disposalCount}>
+                    For Disposal
+                  </Badge>
+                }
+                value="7"
+                className={value === "7" ? "tab__background" : null}
+              />
             )}
           </Tabs>
 
