6640d1e0f9ac6dfceb8512ca26bf9620d8bbd806
Jampord <cs.actubjohnford@gmail.com>
2026-02-16 14:20:44 +0800

    feat: add Repair Confirmation component with tabbed interface and notification handling

---
+++ src/Pages/Asset Movement/Pullout Confirmation/RepairConfirmation.jsx
@@ -0,0 +1,55 @@
+import { TabContext, TabPanel } from "@mui/lab";
+import { Badge, Box, Tab, Tabs, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useGetNotificationApiQuery } from "../../../Redux/Query/Notification";
+import Repaired from "./Repaired";
+
+const RepairConfirmation = () => {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const { data: notifData, refetch } = useGetNotificationApiQuery();
+
+  useEffect(() => {
+    refetch();
+  }, [notifData]);
+
+  return (
+    <Box className="mcontainer">
+      <Typography className="mcontainer__title" sx={{ fontFamily: "Anton", fontSize: "1.6rem" }}>
+        Repair Confirmation
+      </Typography>
+
+      <Box>
+        <TabContext value={value}>
+          <Tabs onChange={handleChange} value={value}>
+            <Tab
+              label={
+                <Badge color="error" badgeContent={notifData?.repairedCount}>
+                  To Receive
+                </Badge>
+              }
+              value="1"
+              className={value === "1" ? "tab__background" : null}
+            />
+
+            <Tab label="Received" value="2" className={value === "2" ? "tab__background" : null} />
+          </Tabs>
+
+          <TabPanel sx={{ p: 0 }} value="1" index="1">
+            <Repaired received={0} />
+          </TabPanel>
+
+          <TabPanel sx={{ p: 0 }} value="2" index="2">
+            <Repaired received={1} />
+          </TabPanel>
+        </TabContext>
+      </Box>
+    </Box>
+  );
+};
+
+export default RepairConfirmation;
